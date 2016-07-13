9c6231c8e9090731fe4b3ad0b771a29470e31444
Sergey Kokorin <sergey.kokorin@zalando.de>
2016-07-14 00:38:53 +0200

    test for derivative

--- src/test/scala/expr/Derivative.scala
+++ src/test/scala/expr/Derivative.scala
@@ -1,16 +1,7 @@
-import org.scalatest._
-
-/*import util.Random
-import util.Random.nextDouble
-
-import galileo.complex._
-import galileo.environment._
+import galileo.environment.Environment
 import galileo.expr._
-import galileo.exprhandler._
-*/
-import galileo.expr._
-import galileo.manipulate.Simplify
 import galileo.trigonometry.SinF1
+import org.scalatest._
 
 class DerivativeTest extends FunSuite {
 
@@ -19,8 +10,20 @@ class DerivativeTest extends FunSuite {
 	val g = SinF1( x )
 	val h = Square( x )
 	val f = Fraction( g, h )
-	test( "fraction") {
+  test("fraction") {
 		val fd = Derivative( f, x )
 		assert( fd.visit().simplify.toString == "(x*cos(x)-2.0*sin(x))/x^3.0" )
 	}
+
+  test("Laplace transform derivatives for Exponential distribution at 0 is its moments with factorial coefs") {
+    val lambda = Variable("lambda")
+    val t = Variable("t")
+    val laplaseTransform = Fraction(lambda, Diff(lambda, t))
+    val moment = Derivative(laplaseTransform, t)
+    val env = new Environment(Option.empty)
+    env.set(t.name, Number(0))
+    env.set(lambda.name, Number(1))
+    assert(laplaseTransform.eval().visit(Option(env)) == Number(1.0))
+    assert(moment.visit().visit(Option(env)) == Number(1.0))
+  }
 }
